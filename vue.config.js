d65eb647df08463ce02765e65bda9e4f3a242a57
jakesharma <jaysuthar597@gmail.com>
2024-11-13 18:11:01 +0530

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: "/portfolio"
 })
 
 // vue.config.js
