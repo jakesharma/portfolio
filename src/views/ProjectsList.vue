b73b9af589e0c15eac4101abd9491a7da0820554
jakesharma <jaysuthar597@gmail.com>
2024-11-13 19:25:10 +0530

    update

--- src/views/ProjectsList.vue
+++ src/views/ProjectsList.vue
@@ -52,9 +52,9 @@
 
       <div class="col">
         <div class="card shadow-sm border-0 text-white">
-          <a href="https://jakesharma.github.io/circle/" target="_blank" class="w-inline-block" style="">
-            <img src="../assets/image/website-screenshot/circle.png" class="img-fluid p-3" style="border-radius: 20px;"
-              alt="project" loading="lazy">
+          <a href="https://jakesharma.github.io/ZoomStudio/" target="_blank" class="w-inline-block" style="">
+            <img src="../assets/image/website-screenshot/ZoomStudio.png" class="img-fluid p-3"
+              style="border-radius: 20px;" alt="project" loading="lazy">
           </a>
 
           <div class=" card-body ">
@@ -63,7 +63,93 @@
                 <a href="" class="nav-link">
                   <div class="row">
                     <div class="col-10 text-start ">
-                      <p class="">circle</p>
+                      <p class="">myportfolio</p>
+                    </div>
+                    <div class="col-2 text-end">
+                      <i class="fa-solid fa-arrow-right"></i>
+                    </div>
+                  </div>
+                </a>
+              </h5>
+              <div class="card-text ">
+                <div class="d-flex">
+                  <div class="">
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png"
+                      alt="Flag" class="img-fluid" width="20px" loading="lazy">
+                  </div>
+                  <div class="ms-2">
+                    <p>India</p>
+                  </div>
+                </div>
+                <div class="d-flex">
+                  <p>Developed by:</p>
+                  <p class="ms-2">Jai kishan suthar</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card shadow-sm border-0 text-white">
+          <a href="https://jakesharma.github.io/Web.Resume/" target="_blank" class="w-inline-block" style="">
+            <img src="../assets/image/website-screenshot/Web.Resume.png" class="img-fluid p-3"
+              style="border-radius: 20px;" alt="project" loading="lazy">
+          </a>
+
+          <div class=" card-body ">
+            <div class="card-2-color px-3 pt-3">
+              <h5 class="card-title">
+                <a href="" class="nav-link">
+                  <div class="row">
+                    <div class="col-10 text-start ">
+                      <p class="">myportfolio</p>
+                    </div>
+                    <div class="col-2 text-end">
+                      <i class="fa-solid fa-arrow-right"></i>
+                    </div>
+                  </div>
+                </a>
+              </h5>
+              <div class="card-text ">
+                <div class="d-flex">
+                  <div class="">
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png"
+                      alt="Flag" class="img-fluid" width="20px" loading="lazy">
+                  </div>
+                  <div class="ms-2">
+                    <p>India</p>
+                  </div>
+                </div>
+                <div class="d-flex">
+                  <p>Developed by:</p>
+                  <p class="ms-2">Jai kishan suthar</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card shadow-sm border-0 text-white">
+          <a href="https://jakesharma.github.io/Coding-Knowledge/" target="_blank" class="w-inline-block" style="">
+            <img src="../assets/image/website-screenshot/Coding-Knowledge.png" class="img-fluid p-3"
+              style="border-radius: 20px;" alt="project" loading="lazy">
+          </a>
+
+          <div class=" card-body ">
+            <div class="card-2-color px-3 pt-3">
+              <h5 class="card-title">
+                <a href="" class="nav-link">
+                  <div class="row">
+                    <div class="col-10 text-start ">
+                      <p class="">myportfolio</p>
                     </div>
                     <div class="col-2 text-end">
                       <i class="fa-solid fa-arrow-right"></i>
@@ -150,6 +236,49 @@
                   <div class="row">
                     <div class="col-10 text-start ">
                       <p class="">Jai Kishan Suthar</p>
+                    </div>
+                    <div class="col-2 text-end">
+                      <i class="fa-solid fa-arrow-right"></i>
+                    </div>
+                  </div>
+                </a>
+              </h5>
+              <div class="card-text ">
+                <div class="d-flex">
+                  <div class="">
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png"
+                      alt="Flag" class="img-fluid" width="20px" loading="lazy">
+                  </div>
+                  <div class="ms-2">
+                    <p>India</p>
+                  </div>
+                </div>
+                <div class="d-flex">
+                  <p>Developed by:</p>
+                  <p class="ms-2">Jai kishan suthar</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card shadow-sm border-0 text-white">
+          <a href="https://jakesharma.github.io/circle/" target="_blank" class="w-inline-block" style="">
+            <img src="../assets/image/website-screenshot/circle.png" class="img-fluid p-3" style="border-radius: 20px;"
+              alt="project" loading="lazy">
+          </a>
+
+          <div class=" card-body ">
+            <div class="card-2-color px-3 pt-3">
+              <h5 class="card-title">
+                <a href="" class="nav-link">
+                  <div class="row">
+                    <div class="col-10 text-start ">
+                      <p class="">circle</p>
                     </div>
                     <div class="col-2 text-end">
                       <i class="fa-solid fa-arrow-right"></i>
