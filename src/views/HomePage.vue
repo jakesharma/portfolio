c9943e095d1c648aa9fb6971d6fb54f4327890aa
jakesharma <jaysuthar597@gmail.com>
2025-01-31 23:52:11 +0530

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,28 +1,31 @@
 <template>
-    <div class="pb-5  banner" style="overflow-x: hidden;">
+    <div class="pb-5 banner" style="overflow-x: hidden;">
         <div class="row d-flex justify-content-between align-items-center py-0">
             <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 
                         d-flex justify-content-xl-start justify-content-center 
                         order-xxl-first order-xl-first order-lg-first order-md-first order-sm-last order-last "
-                        data-aos="fade-left" data-aos-duration="1000">
+                data-aos="fade-left" data-aos-duration="1000">
                 <div class="px-xxl-5 px-xl-5 px-lg-5 px-md-5 px-3 left-side-info ">
                     <div class="text-info CodeDesign">
                         <p class="text-success" style="font-family: cursive; font-style: italic;">&lt;/head&gt;</p>
                         <p class="text-success" style="font-family: cursive; font-style: italic;">&lt;body&gt;</p>
                         <p class="text-success ps-4" style="font-family: cursive; font-style: italic;">&lt;h1&gt;</p>
-                        <h1 class="ps-5 text-light MyNameInfo"
-                            style="font-family: sans-serif; font-weight:700; ">
+                        <h1 class="ps-5 text-light MyNameInfo" style="font-family: sans-serif; font-weight:700; ">
                             <p class="">Hi,</p>
                             <p>I'm <span class="text-warning">Jai,</span></p>
                             <p>web developer</p>
                         </h1>
                         <p class="text-success ps-4" style="font-family: cursive; font-style: italic;">&lt;/h1&gt;</p>
                         <div class="fs-6 ps-4">
-                            <p><span class="text-success"
-                                    style="font-family: cursive; font-style: italic;">&lt;p&gt;</span><span
-                                    style="color: white; font-family: sans-serif ;"> Front End
-                                    developer / Web Design </span><span
-                                    style="color: green; font-family: cursive; font-style: italic;">&lt;/p&gt;</span>
+                            <p><span class="text-success" style="font-family: cursive; font-style: italic;">
+                                    &lt;p&gt;
+                                </span>
+                                <span class="text-light" style="font-family: sans-serif ;">
+                                    Front End developer / Web Design
+                                </span>
+                                <span style="color: green; font-family: cursive; font-style: italic;">
+                                    &lt;/p&gt;
+                                </span>
                             </p>
                         </div>
                         <div class="ps-4">
